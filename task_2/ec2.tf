2a7db1cb0c3e959467d8825fc8fb92fb77082e95
Kerem AR <keremar0000@gmail.com>
2025-07-24 11:46:15 +0300

    fix

--- task_2/ec2.tf
+++ task_2/ec2.tf
@@ -1,7 +1,7 @@
-resource "aws_instance" "cmtr-zdv1y551-ec2" {
+resource "aws_instance" "instance" {
   ami           = data.aws_ami.amazon_linux_2.id
   instance_type = var.instance_type
-  key_name      = aws_key_pair.cmtr-zdv1y551-keypair.key_name
+  key_name      = aws_key_pair.keypair.key_name
   subnet_id     = data.aws_subnet.lab_public_subnet.id
 
   vpc_security_group_ids = [data.aws_security_group.lab_sg.id]
