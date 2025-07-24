819434e7df7a349d6c1fe8619e9ebfd66da9bccf
Kerem AR <keremar0000@gmail.com>
2025-07-24 13:00:00 +0300

    fmt

--- task_2/ec2.tf
+++ task_2/ec2.tf
@@ -1,15 +1,12 @@
 resource "aws_instance" "instance" {
   ami           = data.aws_ami.amazon_linux_2.id
   instance_type = var.instance_type
-  key_name      = aws_key_pair.keypair.key_name
+  key_name      = var.key_name
   subnet_id     = data.aws_subnet.lab_public_subnet.id
 
-  vpc_security_group_ids = [data.aws_security_group.lab_sg.id]
+  security_groups = [var.sg_name_filter]
 
-  tags = merge(
-    local.common_tags,
-    {
-      Name = var.instance_name
-    }
-  )
+  tags = {
+    Name = var.instance_name
+  }
 } 
